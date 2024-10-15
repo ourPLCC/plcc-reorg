144a2da775e251dd294c761fe8d1b2e8fffa99cb
Connor Francis <cfster03@gmail.com>
2024-10-15 16:31:00 -0400

    feat: lhs format checking

--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import re
 from ...load_rough_spec.parse_lines import Line
-from ...parse_spec.parse_syntactic_spec import SyntacticSpec, SyntacticRule
+from ...parse_spec.parse_syntactic_spec import SyntacticSpec, SyntacticRule, LhsNonTerminal
 
 
 @dataclass
@@ -20,23 +20,50 @@ class SyntacticValidator:
     def __init__(self, syntacticSpec: SyntacticSpec):
         self.syntacticSpec = syntacticSpec
         self.errorList = []
+        self.nonTerminals = set()
 
     def validate(self) -> list:
         if not self.syntacticSpec:
             return self.errorList
         for rule in self.syntacticSpec:
-            self._handle(rule)
+            self._handleLHS(rule)
+        # for rule in self.syntacticSpec:
+            # self._handleRHS(rule)
         return self.errorList
 
-    def _handle(self, rule: SyntacticRule | Line):
+    def _handleLHS(self, rule: SyntacticRule | Line):
         if isinstance(rule, SyntacticRule):
-            print("todo!")
-            # self.checkRuleOne(rule)
-            # self._checkRuleTwo(rule)
-            # self._checkRuleThree(rule)
+            self._checkLHS(rule)
         else:
             self._checkForLine(rule)
+
+    def _checkLHS(self, rule: SyntacticRule):
+        resolved_name = rule.lhs.name
+        self._checkLHSNonTerminalName(rule)
+        if rule.lhs.altName:
+            self._checkLHSNonTerminalAltName(rule)
+            resolved_name = rule.lhs.altName
+        self._appendNonTerminals(resolved_name)
+
+    def _checkLHSNonTerminalName(self, rule: SyntacticRule):
+        if not re.match(r"^[a-z][a-zA-Z0-9_]+$", rule.lhs.name):
+            self._appendInvalidLhsNameError(rule)
+
+    def _checkLHSNonTerminalAltName(self, rule: SyntacticRule):
+        if not re.match(r"^[A-Z][a-zA-Z0-9_]+$", rule.lhs.altName):
+            self._appendInvalidLhsAltNameError(rule)
+
+    def _appendNonTerminals(self, resolved_name: str):
+        pass
 
     def _checkForLine(self, line: Line):
         message = f"Invalid rule format found on line: {line.number}"
         self.errorList.append(ValidationError(line=line, message=message))
+
+    def _appendInvalidLhsNameError(self, rule: SyntacticRule):
+        message = f"Invalid LHS name format for rule: '{rule.line.string}' (must start with a lower-case letter, and may contain upper or lower case letters, numbers and/or underscore) on line: {rule.line.number}"
+        self.errorList.append(ValidationError(line=rule.line, message=message))
+
+    def _appendInvalidLhsAltNameError(self, rule: SyntacticRule):
+        message = f"Invalid LHS alternate name format for rule: '{rule.line.string}' (must start with a upper-case letter, and may contain upper or lower case letters, numbers and/or underscore) on line: {rule.line.number}"
+        self.errorList.append(ValidationError(line=rule.line, message=message))
--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec_test.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec_test.py
@@ -3,7 +3,7 @@ from typing import List
 
 from ...load_rough_spec.parse_lines import Line
 from .validate_syntactic_spec import ValidationError, validate_syntactic_spec
-from ...parse_spec.parse_syntactic_spec import SyntacticRule, SyntacticSpec, Symbol, LhsNonTerminal
+from ...parse_spec.parse_syntactic_spec import SyntacticRule, SyntacticSpec, Symbol, LhsNonTerminal, Terminal
 
 
 def test_empty_no_errors():
@@ -18,6 +18,94 @@ def test_None_no_errors():
     assert len(errors) == 0
 
 
+def test_valid_line_no_errors():
+    line = makeLine("<sentence> ::= WORD WORD WORD")
+    terminal = makeTerminal("WORD")
+    valid_spec = [
+        makeSyntacticRule(
+            line,
+            makeLhsNonTerminal("sentence"),
+            [terminal, terminal, terminal]
+        )
+    ]
+    errors = validate_syntactic_spec(valid_spec)
+    assert len(errors) == 0
+
+
+def test_valid_lhs_alt_name():
+    line = makeLine("<sentence>:Name_Version_1 ::= WORD WORD WORD")
+    terminal = makeTerminal("WORD")
+    spec = [
+        makeSyntacticRule(
+            line,
+            makeLhsNonTerminal("sentence", "Name_Version_1"),
+            [terminal, terminal, terminal]
+        )
+    ]
+    errors = validate_syntactic_spec(spec)
+    assert len(errors) == 0
+
+
+def test_number_lhs_terminal():
+    line = makeLine("<1sentence> ::= WORD WORD WORD")
+    terminal = makeTerminal("WORD")
+    spec = [
+        makeSyntacticRule(
+            line,
+            makeLhsNonTerminal("1sentence"),
+            [terminal, terminal, terminal]
+        )
+    ]
+    errors = validate_syntactic_spec(spec)
+    assert len(errors) == 1
+    assert errors[0] == makeInvalidLhsNameFormatError(spec[0])
+
+
+def test_capital_lhs_terminal():
+    line = makeLine("<Sentence> ::= WORD WORD WORD")
+    terminal = makeTerminal("WORD")
+    spec = [
+        makeSyntacticRule(
+            line,
+            makeLhsNonTerminal("Sentence"),
+            [terminal, terminal, terminal]
+        )
+    ]
+    errors = validate_syntactic_spec(spec)
+    assert len(errors) == 1
+    assert errors[0] == makeInvalidLhsNameFormatError(spec[0])
+
+
+def test_undercase_lhs_alt_name():
+    line = makeLine("<sentence>:name ::= WORD WORD WORD")
+    terminal = makeTerminal("WORD")
+    spec = [
+        makeSyntacticRule(
+            line,
+            makeLhsNonTerminal("sentence", "name"),
+            [terminal, terminal, terminal]
+        )
+    ]
+    errors = validate_syntactic_spec(spec)
+    assert len(errors) == 1
+    assert errors[0] == makeInvalidLhsAltNameFormatError(spec[0])
+
+
+def test_underscore_lhs_alt_name():
+    line = makeLine("<sentence>:_name ::= WORD WORD WORD")
+    terminal = makeTerminal("WORD")
+    spec = [
+        makeSyntacticRule(
+            line,
+            makeLhsNonTerminal("sentence", "_name"),
+            [terminal, terminal, terminal]
+        )
+    ]
+    errors = validate_syntactic_spec(spec)
+    assert len(errors) == 1
+    assert errors[0] == makeInvalidLhsAltNameFormatError(spec[0])
+
+
 def makeSyntacticSpec(ruleList=None):
     return SyntacticSpec(ruleList)
 
@@ -30,9 +118,23 @@ def makeLine(string, lineNumber=1, file=None):
     return Line(string, lineNumber, file)
 
 
+def makeLhsNonTerminal(name: str | None, altName: str | None = None):
+    return LhsNonTerminal(name, altName)
+
+
+def makeTerminal(name: str | None):
+    return Terminal(name)
+
+
 def makeValidationError(line, message):
     return ValidationError(line, message)
 
-def makeInvalidTerminalFormatError(rule):
-    message = f"Invalid terminal format for rule: '{rule.line.str}' (must be all upper-case letters, numbers, and underscore and cannot start with a number) on line: {rule.line.number}"
+
+def makeInvalidLhsNameFormatError(rule):
+    message = f"Invalid LHS name format for rule: '{rule.line.string}' (must start with a lower-case letter, and may contain upper or lower case letters, numbers and/or underscore) on line: {rule.line.number}"
+    return makeValidationError(rule.line, message)
+
+
+def makeInvalidLhsAltNameFormatError(rule):
+    message = f"Invalid LHS alternate name format for rule: '{rule.line.string}' (must start with a upper-case letter, and may contain upper or lower case letters, numbers and/or underscore) on line: {rule.line.number}"
     return makeValidationError(rule.line, message)
