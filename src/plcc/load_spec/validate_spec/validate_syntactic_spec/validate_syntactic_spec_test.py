59fe01044328d6269d3a609457fb82ac2b0b086c
Connor <cf620152@wne.edu>
2024-10-15 13:48:33 -0400

    wip validating syntactic spec
    this commit is going to be squashed soon. I am switching workspaces.

--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec_test.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec_test.py
@@ -1,8 +1,9 @@
 from pytest import raises, mark, fixture
+from typing import List
 
 from ...load_rough_spec.parse_lines import Line
 from .validate_syntactic_spec import ValidationError, validate_syntactic_spec
-from ...parse_spec.parse_syntactic_spec import SyntacticRule, SyntacticSpec
+from ...parse_spec.parse_syntactic_spec import SyntacticRule, SyntacticSpec, Symbol, LhsNonTerminal
 
 
 def test_empty_no_errors():
@@ -11,12 +12,18 @@ def test_empty_no_errors():
     assert len(errors) == 0
 
 
+def test_None_no_errors():
+    syntacticSpec = makeSyntacticSpec(None)
+    errors = validate_syntactic_spec(syntacticSpec)
+    assert len(errors) == 0
+
+
 def makeSyntacticSpec(ruleList=None):
     return SyntacticSpec(ruleList)
 
 
-def makeSyntacticRule(line, lhs, rhs):
-    return SyntacticRule(line, lhs, rhs)
+def makeSyntacticRule(line: Line, lhs: LhsNonTerminal, rhsList: List[Symbol]):
+    return SyntacticRule(line, lhs, rhsList)
 
 
 def makeLine(string, lineNumber=1, file=None):
@@ -25,3 +32,7 @@ def makeLine(string, lineNumber=1, file=None):
 
 def makeValidationError(line, message):
     return ValidationError(line, message)
+
+def makeInvalidTerminalFormatError(rule):
+    message = f"Invalid terminal format for rule: '{rule.line.str}' (must be all upper-case letters, numbers, and underscore and cannot start with a number) on line: {rule.line.number}"
+    return makeValidationError(rule.line, message)
