7a62f190f67d6d77171df303a4674f50dc8edad7
AksharP5 <aksharpatel1233@gmail.com>
2024-10-16 21:40:33 -0400

    (LL1Wrapper): update __eq__ to account for None objects

--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/LL1Wrapper_and_SpecGrammar.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/LL1Wrapper_and_SpecGrammar.py
@@ -9,6 +9,8 @@ class LL1Wrapper:
         self.specObject = specObject
 
     def __eq__(self, other):
+        if self.specObject is None or other.specObject is None:
+            return self is other
         return isinstance(other, LL1Wrapper) and self.name == other.name
 
     def __hash__(self):
