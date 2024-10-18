041848f912cb0021a6cb9551d2185d19f7316465
AksharP5 <aksharpatel1233@gmail.com>
2024-10-17 22:20:05 -0400

    (LL1Wrapper): add tests for Terminal and CapturingTerminal

--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/LL1Wrapper_test.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/LL1Wrapper_test.py
@@ -1,39 +1,50 @@
 from pytest import raises, mark, fixture
 from .LL1Wrapper import LL1Wrapper
 from plcc.load_spec.parse_spec.parse_syntactic_spec.structs import (
-    LhsNonTerminal, RhsNonTerminal
+    LhsNonTerminal, RhsNonTerminal, Terminal, CapturingTerminal
 )
 
 @fixture
-def lhsNonTerminal():
-    return LhsNonTerminal(getDefaultNonterminalName())
+def lhsWrapped():
+    return LL1Wrapper(getDefaultNonterminalName(), getLhsNonTerminal())
 
 @fixture
-def rhsNonTerminal():
-    return RhsNonTerminal(getDefaultNonterminalName())
+def rhsWrapped():
+    return LL1Wrapper(getDefaultNonterminalName(), getRhsNonTerminal())
 
 @fixture
-def lhsWrapped(lhsNonTerminal):
-    return LL1Wrapper(getDefaultNonterminalName(), lhsNonTerminal)
+def terminalWrapped():
+    return LL1Wrapper('TERMINAL', getTerminal())
 
 @fixture
-def rhsWrapped(rhsNonTerminal):
-    return LL1Wrapper(getDefaultNonterminalName(), rhsNonTerminal)
+def capturingTerminalWrapped():
+    return LL1Wrapper('TERMINAL', getCapturingTerminal())
 
-def test_init(lhsWrapped, lhsNonTerminal):
+def test_init(lhsWrapped):
     assert lhsWrapped.name == getDefaultNonterminalName()
-    assert lhsWrapped.specObject == lhsNonTerminal
+    assert lhsWrapped.specObject == getLhsNonTerminal()
 
-def test_eq_with_same_name_and_object(lhsWrapped, lhsNonTerminal):
-    otherWrapped = createWrapper(lhsWrapped.name, lhsNonTerminal)
+def test_nonterminal_eq_with_same_name_and_object(lhsWrapped):
+    otherWrapped = createWrapper(lhsWrapped.name, lhsWrapped.specObject)
     assert lhsWrapped == otherWrapped
 
-def test_eq_with_same_name_and_diff_object(lhsWrapped, rhsWrapped):
+def test_terminal_eq_with_same_name_and_object(terminalWrapped):
+    otherWrapped = createWrapper(terminalWrapped.name, terminalWrapped.specObject)
+    assert terminalWrapped == otherWrapped
+
+def test_nonterminal_eq_with_same_name_and_diff_object(lhsWrapped, rhsWrapped):
     assert lhsWrapped == rhsWrapped
 
-def test_not_eq_with_diff_name(lhsWrapped):
+def test_terminal_eq_with_same_name_and_diff_object(terminalWrapped, capturingTerminalWrapped):
+    assert terminalWrapped == capturingTerminalWrapped
+
+def test_nonterminal_not_eq_with_diff_name(lhsWrapped):
     otherWrapped = createWrapper(getOtherName(), lhsWrapped.specObject)
     assert lhsWrapped != otherWrapped
+
+def test_terminal_not_eq_with_diff_name(terminalWrapped):
+    otherWrapped = createWrapper(getOtherTerminalName(), terminalWrapped.specObject)
+    assert terminalWrapped != otherWrapped
 
 def test_none_objects_eq_when_same_instance():
     wrapped = createWrappedWithNoneObject(getDefaultNonterminalName())
@@ -49,12 +60,19 @@ def test_not_eq_object_and_none(lhsWrapped):
     otherWrapped = createWrappedWithNoneObject(lhsWrapped.name)
     assert lhsWrapped != otherWrapped
 
-def test_hash_same_name_diff_object(lhsWrapped, rhsWrapped):
+def test_hash_nonterminal_same_name_diff_object(lhsWrapped, rhsWrapped):
     assert hash(lhsWrapped) == hash(rhsWrapped)
 
-def test_hash_diff_name(lhsWrapped):
+def test_hash_terminal_same_name_diff_object(terminalWrapped, capturingTerminalWrapped):
+    assert hash(terminalWrapped) == hash(capturingTerminalWrapped)
+
+def test_hash_nonterminal_diff_name(lhsWrapped):
     otherWrapped = createWrapper(getOtherName(), lhsWrapped.specObject)
     assert hash(lhsWrapped) != hash(otherWrapped)
+
+def test_hash_terminal_diff_name(terminalWrapped):
+    otherWrapped = createWrapper(getOtherTerminalName(), terminalWrapped.specObject)
+    assert hash(terminalWrapped) != hash(otherWrapped)
 
 def test_hash_object_and_none_same_name(lhsWrapped):
     otherWrapped = createWrappedWithNoneObject(lhsWrapped.name)
@@ -73,5 +91,23 @@ def createWrappedWithNoneObject(name):
 def getDefaultNonterminalName():
     return 'nonTerminal'
 
+def getDefaultTerminalName():
+    return 'TERMINAL'
+
 def getOtherName():
     return 'otherName'
+
+def getOtherTerminalName():
+    return 'OTHERTERMINAL'
+
+def getLhsNonTerminal():
+    return LhsNonTerminal(getDefaultNonterminalName())
+
+def getRhsNonTerminal():
+    return RhsNonTerminal(getDefaultNonterminalName())
+
+def getTerminal():
+    return Terminal(getDefaultTerminalName())
+
+def getCapturingTerminal():
+    return CapturingTerminal(getDefaultTerminalName())
