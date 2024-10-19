5cf4a48f5fc8c281e92e7c201f04788dd4142c7b
Connor Francis <cfster03@gmail.com>
2024-10-18 20:34:12 -0400

    lay plumbing for rest of branch

--- src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec.py
+++ src/plcc/load_spec/validate_spec/validate_syntactic_spec/validate_syntactic_spec.py
@@ -1,14 +1,9 @@
-import re
 from ...parse_spec.parse_syntactic_spec import (
     SyntacticSpec,
     SyntacticRule,
 )
-from .errors import (
-    InvalidLhsNameError,
-    InvalidLhsAltNameError,
-    DuplicateLhsError,
-    ValidationError
-)
+from .validate_lhs import validate_lhs
+from .validate_rhs import validate_rhs
 
 
 def validate_syntactic_spec(syntacticSpec: SyntacticSpec):
@@ -31,61 +26,10 @@ class SyntacticValidator:
         return self.errorList
 
     def _validateLhs(self):
-        lhs_error_list, non_terminal_set = SyntacticLhsValidator(
-            self.syntacticSpec.copy()
-        ).validate()
+        lhs_error_list, non_terminal_set = validate_lhs(self.syntacticSpec)
         if lhs_error_list:
             self.errorList = lhs_error_list
         self.nonTerminals = non_terminal_set
 
-
-class SyntacticLhsValidator:
-    spec: SyntacticSpec
-
-    def __init__(self, syntacticSpec: SyntacticSpec):
-        self.spec = syntacticSpec
-        self.errorList = []
-        self.nonTerminals = set()
-
-    def validate(self) -> tuple[list[ValidationError], set[str]]:
-        while len(self.spec) > 0:
-            self.rule = self.spec.pop(0)
-            self._checkLine()
-        return self.errorList, self.nonTerminals
-
-    def _checkLine(self):
-        name, alt_name = self._getNames()
-        self._checkName(name)
-        if alt_name:
-            self._checkAltName(alt_name)
-        self._checkDuplicates()
-
-    def _getNames(self) -> tuple[str, str]:
-        return (self.rule.lhs.name, self.rule.lhs.altName)
-
-    def _checkName(self, name: str):
-        if not re.match(r"^[a-z][a-zA-Z0-9_]+$", name):
-            self._appendInvalidLhsNameError()
-
-    def _checkAltName(self, alt_name: str):
-        if not re.match(r"^[A-Z][a-zA-Z0-9_]+$", alt_name):
-            self._appendInvalidLhsAltNameError()
-
-    def _getResolvedName(self) -> str:
-        name, alt_name = self._getNames()
-        return alt_name if alt_name else name.capitalize()
-
-    def _checkDuplicates(self):
-        name = self._getResolvedName()
-        if name in self.nonTerminals:
-            self._appendDuplicateLhsError()
-        self.nonTerminals.add(name)
-
-    def _appendInvalidLhsNameError(self):
-        self.errorList.append(InvalidLhsNameError(self.rule))
-
-    def _appendInvalidLhsAltNameError(self):
-        self.errorList.append(InvalidLhsAltNameError(self.rule))
-
-    def _appendDuplicateLhsError(self):
-        self.errorList.append(DuplicateLhsError(self.rule))
+    def _validateRhs(self):
+        _ = validate_rhs(self.syntacticSpec)
