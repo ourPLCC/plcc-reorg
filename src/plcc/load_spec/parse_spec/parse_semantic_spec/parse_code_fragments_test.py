0e47eb70459c92ad125aff4a1a2dadeb58e0402b
michaelBanerjee <98488712+michaelBanerjee@users.noreply.github.com>
2025-02-05 19:53:26 +0000

    add test coverage for parse_code_fragments

--- src/plcc/load_spec/parse_spec/parse_semantic_spec/parse_code_fragments_test.py
+++ src/plcc/load_spec/parse_spec/parse_semantic_spec/parse_code_fragments_test.py
@@ -20,8 +20,8 @@ def test_input_must_be_Lines_and_Blocks():
     with raises(TypeError):
         parse_code_fragments(invalid_input)
 
-def test_blank_lines_ignored():
-    lines = [make_line('Class:init'), make_line('\n'), make_line(''), make_block()]
+def test_empty_lines_ignored():
+    lines = [make_line('Class:init'), make_line('\n'), make_line(''), None, make_block()]
     assert parse_code_fragments(lines) == [
         CodeFragment(make_target_locator(lines[0], 'Class', 'init'), make_block())]
 
@@ -44,6 +44,14 @@ def test_single_block_parses_undefined_target_locator():
         CodeFragment(None, lines_and_blocks[0])
     ]
 
+def test_consecutive_blocks_parse_undefined_target_lacators():
+    blocks = [make_block(), make_block()]
+    code_fragments = parse_code_fragments(blocks)
+    assert code_fragments == [
+        CodeFragment(None, blocks[0]),
+        CodeFragment(None, blocks[1])
+    ]
+
 def test_mismatched_target_locator_and_block_make_two_code_fragments():
     lines_and_blocks = [make_block(), make_line('Class:init')]
     code_fragments = parse_code_fragments(lines_and_blocks)
@@ -51,7 +59,6 @@ def test_mismatched_target_locator_and_block_make_two_code_fragments():
         CodeFragment(None, lines_and_blocks[0]),
         CodeFragment(make_target_locator(lines_and_blocks[1], 'Class', 'init'), None)
     ]
-
 
 def test_missing_blocks_are_defined_as_None():
     lines_and_blocks = [make_line('Class:init')]
